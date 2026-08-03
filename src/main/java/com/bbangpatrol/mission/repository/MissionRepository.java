ec21eeeb9f8ad1ca4aed68ae767a401e69010a4e
황우찬 <cmy397264@naver.com>
2026-08-03 20:17:16 +0900

    refact: 미션 최근 갱신순으로 조회

--- src/main/java/com/bbangpatrol/mission/repository/MissionRepository.java
+++ src/main/java/com/bbangpatrol/mission/repository/MissionRepository.java
@@ -16,20 +16,20 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
     // filter=all
     @Query("SELECT m, mp FROM Mission m LEFT JOIN MissionProgress mp " +
             "ON mp.mission = m AND mp.user.id = :userId " +
-            "WHERE (:cursor IS NULL OR m.id > :cursor) ORDER BY m.id ASC")
+            "WHERE (:cursor IS NULL OR m.id > :cursor) ORDER BY mp.updatedAt ASC")
     List<Object[]> findAllWithProgress(Long userId, Long cursor, Pageable pageable);
 
     // filter=in-progress (progress 없음 = 시작 안 함도 포함)
     @Query("SELECT m, mp FROM Mission m LEFT JOIN MissionProgress mp " +
             "ON mp.mission = m AND mp.user.id = :userId " +
             "WHERE (mp IS NULL OR mp.status = :inProgress) " +
-            "AND (:cursor IS NULL OR m.id > :cursor) ORDER BY m.id ASC")
+            "AND (:cursor IS NULL OR m.id > :cursor) ORDER BY mp.updatedAt ASC")
     List<Object[]> findInProgress(Long userId, MissionStatus inProgress, Long cursor, Pageable pageable);
 
     // filter=completed
     @Query("SELECT m, mp FROM Mission m LEFT JOIN MissionProgress mp " +
             "ON mp.mission = m AND mp.user.id = :userId " +
             "WHERE mp.status IN :statuses " +
-            "AND (:cursor IS NULL OR m.id > :cursor) ORDER BY m.id ASC")
+            "AND (:cursor IS NULL OR m.id > :cursor) ORDER BY mp.updatedAt ASC")
     List<Object[]> findByStatuses(Long userId, List<MissionStatus> statuses, Long cursor, Pageable pageable);
 }
