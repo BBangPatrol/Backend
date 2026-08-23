60dd93a3a5af4c76745092d5e0c6918a7bad4305
jamonda1 <park00seungjin@gmail.com>
2026-08-23 23:33:12 +0900

    feat: 영수증을 통한 방문인증 로직 완성

--- src/main/java/com/bbangpatrol/visit/repository/VisitRepository.java
+++ src/main/java/com/bbangpatrol/visit/repository/VisitRepository.java
@@ -5,7 +5,11 @@ import com.bbangpatrol.visit.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VisitRepository extends JpaRepository<Visit, Long> {
     List<Visit> findByUser(User user);
+
+    // 사용자 + 빵집 조회에 사용
+    Optional<Visit> findByUserIdAndBakeryId(Long userId, Long bakeryId);
 }
