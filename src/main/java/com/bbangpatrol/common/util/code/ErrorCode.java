05164a7f8ef023ad8ba0e38c6326973b9cfc5f21
황우찬 <cmy397264@naver.com>
2026-09-07 20:25:35 +0900

    fix: 업로드 용량 초과 시 500이 발생하던 문제 수정 및 한도 상향

--- src/main/java/com/bbangpatrol/common/util/code/ErrorCode.java
+++ src/main/java/com/bbangpatrol/common/util/code/ErrorCode.java
@@ -59,6 +59,7 @@ public enum ErrorCode implements BaseErrorCode{
     FORBIDDEN_403(HttpStatus.FORBIDDEN, "COMMON403", "접근이 금지되었습니다"),
     NOT_FOUND_404(HttpStatus.NOT_FOUND, "COMMON404", "요청한 자원을 찾을 수 없습니다"),
     UNSUPPORTED_MEDIA_TYPE415(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "COMMO405", "지원하지 않는 파일형식입니다."),
+    UPLOAD_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "COMMON413", "업로드 용량이 너무 큽니다. 사진은 장당 15MB, 한 번에 60MB 까지 가능합니다."),
     INTERNAL_SERVER_ERROR_500(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 내부 오류가 발생했습니다");
 
     private final HttpStatus status;
